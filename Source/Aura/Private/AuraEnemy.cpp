99d79f295eae41eb3b5c062f1dfa0351869df567
Nick Mac <nick.macdonan@gmail.com>
2024-08-15 20:36:22 -0700

    postprocess

--- Source/Aura/Private/AuraEnemy.cpp
+++ Source/Aura/Private/AuraEnemy.cpp
@@ -3,12 +3,23 @@
 
 #include "AuraEnemy.h"
 
+#include "Aura/Aura.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility,ECR_Block);
+}
+
 void AAuraEnemy::HighlightActor()
 {
-	bShouldHighlight = true;
+	GetMesh()->SetRenderCustomDepth(true);
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	Weapon->SetRenderCustomDepth(true);
+	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-	bShouldHighlight = false;
+	GetMesh()->SetRenderCustomDepth(false);
+	Weapon->SetRenderCustomDepth(false);
 }
